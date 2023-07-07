1599405197e6c8d208595befe2809c5d8d69c7d4
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-07 16:06:28 -0300

    feat(demo): create a demo example

---
+++ demo/main.ts
@@ -0,0 +1,13 @@
+import { Configuration } from './configuration.env';
+
+function main() {
+  const config = new Configuration();
+  //console.log(process.env);
+  console.log(config, {
+    bool: config.bool,
+    num: config.num,
+    text: config.text,
+  });
+}
+
+main();
