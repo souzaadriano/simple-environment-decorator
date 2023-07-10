093524a43141946dd704047390abd47650055dc1
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-10 09:59:30 -0300

    feat(transform-strategies): created strategies to transform into string and number list

---
+++ src/core/use-cases/transform/strategies/number-array.strategy.ts
@@ -0,0 +1,20 @@
+import { ITransformStrategy } from './transform.strategy.contract';
+
+export class NumberArrayStrategy implements ITransformStrategy<number[]> {
+  private readonly _splitBy: string;
+  constructor(splitBy?: string) {
+    this._splitBy = splitBy ?? ';';
+  }
+
+  handle(value: string): number[] {
+    const splited = value.split(this._splitBy);
+    const result = splited.map((item) => Number(item.trim()));
+    result.forEach((item, idx) => this._validateIsNaN(item, splited[idx]));
+    return result;
+  }
+
+  private _validateIsNaN(value: number, originValue: string) {
+    if (!Number.isNaN(value)) return;
+    throw new Error(`item ${originValue} is not a number`);
+  }
+}
---
+++ src/core/use-cases/transform/strategies/string-array.strategy.ts
@@ -0,0 +1,21 @@
+import { ITransformStrategy } from './transform.strategy.contract';
+
+export class StringArrayStrategy implements ITransformStrategy<string[]> {
+  private readonly _config: TStringArrayConfig;
+
+  constructor(config: TStringArrayConfig) {
+    this._config = config ?? { accpetWhiteSpaces: false, splitBy: ';' };
+  }
+
+  handle(value: string): string[] {
+    const { accpetWhiteSpaces, splitBy } = this._config;
+    const result = value.split(splitBy);
+
+    return accpetWhiteSpaces ? result.map((value) => value.trim()) : result;
+  }
+}
+
+type TStringArrayConfig = {
+  splitBy: string;
+  accpetWhiteSpaces?: boolean;
+};
