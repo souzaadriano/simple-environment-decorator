6ee9f2c5345918782e1a0bb3c1d3432bcbc5f3a8
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-12 18:03:24 -0300

    refactor(transform.decorator): improve transform decorators

--- src/presentation/transform.decorator.ts
+++ src/presentation/transform.decorator.ts
@@ -1,6 +1,5 @@
 import { NumberArrayStrategy, StringArrayStrategy } from '@/core/use-cases/transform/strategies';
 import { DecoratorFactory } from '@/factories/decorator.factory';
 
-export const ToStringArray = DecoratorFactory.transformFactory(StringArrayStrategy);
-export const ToNumberArray = DecoratorFactory.transformFactory(NumberArrayStrategy);
-export const transformDecoratorFactory = DecoratorFactory.transformFactory;
+export const ToStringArray = DecoratorFactory.transform(StringArrayStrategy);
+export const ToNumberArray = DecoratorFactory.transform(NumberArrayStrategy);
