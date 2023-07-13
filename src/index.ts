74b349c3ce8410f8d6b05caa46a1e958bdcb87a1
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-13 09:46:44 -0300

    feat(demo): demo updated

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+export * from '@/core/use-cases/transform/strategies/transform.strategy.contract';
+export * from '@/core/use-cases/validatate/strategies/validate.strategy.contract';
 export * from './presentation';
