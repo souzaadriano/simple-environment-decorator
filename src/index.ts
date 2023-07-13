3ee53737067925d9cf612a9bac8fad9f9f42aed1
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-13 10:54:45 -0300

    feat(singleton.decorator): expose singleton decorator on index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from '@/core/use-cases/transform/strategies/transform.strategy.contract';
 export * from '@/core/use-cases/validatate/strategies/validate.strategy.contract';
+export * from './helpers/singleton.decorator';
 export * from './presentation';
