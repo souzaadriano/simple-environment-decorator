cb7ac9c0235a8f2b338f464100342e16bb907f82
Adriano Souza <souzaadriano.contato@gmail.com>
2023-07-19 11:35:08 -0300

    feat(demo): improve demo with custom decorators

--- demo/main.ts
+++ demo/main.ts
@@ -2,13 +2,14 @@ import { Configuration } from './configuration.env';
 
 function main() {
   const config = new Configuration();
-  //console.log(process.env);
   console.log(config, {
     bool: config.bool,
     num: config.num,
     text: config.text,
     textArr: config.textArray,
     numArr: config.numberArray,
+    encodedName: config.encodedName,
+    nodeEnv: config.nodeEnv,
   });
 }
 
